37a759144cf4cb566dc3a63236241d796dd78f6b
Dilip Subramani <dilipkumar.subramani@gmail.com>
2024-05-21 05:16:36 +0530

    first commit

---
+++ discovery-server/src/main/java/com/programming/techie/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.programming.techie.discoveryserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf((csrf1) -> csrf1.ignoringRequestMatchers("/eureka/**"));
+        return httpSecurity.build();
+    }
+}
---
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -0,0 +1,36 @@
+package com.programmingtechie.inventoryservice.service;
+
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    @SneakyThrows //don't use in production should handle exception
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+               ).toList();
+    }
+}
